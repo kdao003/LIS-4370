1242c6ac2c98c026272afb264b19118b16c2e4aa
kdao003 <110855358+kdao003@users.noreply.github.com>
2024-03-25 21:08:16 -0400

    Delete HallmarkGenes directory

--- HallmarkGenes/data-raw/HallmarkGenes.R
+++
@@ -1,8 +0,0 @@
-#data-raw/HallmarkGenes.R
-#dataframe containg enriched signatures table
-
-df <- read.delim("TRUE_TET2_Mutated_EIF2AK3_Enrich_Sig_Table_0vs1.tsv", header = TRUE)
-
-
-
-usethis::use_data(df, overwrite = TRUE)
